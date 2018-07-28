4bd88527241131e0bc2c6758b36692018e89766d
Carmen Collins <hellocarmencollins@gmail.com>
2018-07-27 21:33:09 -0500

    final changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,9 +6,9 @@ var guessesSoFar = "";
 
 var reset = function() {
     guessesLeft = 10;
-    document.getElementById("guessesLeft").innerHTML = guessesLeft;
+    document.getElementById("guessesLeft").textContent = guessesLeft;
     guessesSoFar = "";
-    document.getElementById("guessesSoFar").innerHTML = guessesSoFar;
+    document.getElementById("guessesSoFar").textContent = guessesSoFar;
 };
 
 document.onkeyup = function(e) {
@@ -21,24 +21,24 @@ document.onkeyup = function(e) {
     if (userGuess === appGuess) {
         //Add 1 to Wins
         wins++;
-        document.getElementById("wins").innerHTML = wins;
+        document.getElementById("wins").textContent = wins;
         //And reset the game
         reset();
     //If the user's choice is not the same as the app's choice...
     } else if (userGuess !== appGuess) {
         //Subtract 1 from Guesses Left
         guessesLeft--;
-        document.getElementById("guessesLeft").innerHTML = guessesLeft;
+        document.getElementById("guessesLeft").textContent = guessesLeft;
         //Add the key that the user pressed to Guesses So Far
         guessesSoFar = guessesSoFar + " " + userGuess;
-        document.getElementById("guessesSoFar").innerHTML = guessesSoFar;
+        document.getElementById("guessesSoFar").textContent = guessesSoFar;
     };
 
     //If Guesses So Far equals 0
     if (guessesLeft === 0) {
         //Add 1 to Losses
         losses++;
-        document.getElementById("losses").innerHTML = losses;
+        document.getElementById("losses").textContent = losses;
         //And reset the game
         reset();
     };
